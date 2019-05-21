1dd6acd08c3b159c4abb65ba44c13b23e9a0b61c
Constantinos Constantinides <FDConstantinos@gmail.com>
2019-05-21 11:19:24 +0300

    -Renames field values

--- src/main/java/com/safecharge/model/ExternalToken.java
+++ src/main/java/com/safecharge/model/ExternalToken.java
@@ -6,242 +6,204 @@ package com.safecharge.model;
  * Holder for Cashier's Payment Gateway external token data.
  *
  * @author <a mailto:nikolad@safecharge.com>Nikola Dichev</a>
- * @since 3/10/2017
+ * @since 5/21/2019
  */
 public class ExternalToken {
 
-    private String externalToken_tokenValue;
-    private String externalToken_cardBin;
-    private String externalToken_cardMask;
-    private String externalToken_cardLength;
-    private String externalToken_cardName;
-    private String externalToken_cardExpiration;
-    private String externalToken_cardTypeId;
-    private String externalToken_cardTypeName;
-    private String externalToken_creditCompanyId;
-    private String externalToken_creditCompanyName;
-    private String externalToken_cardBrandId;
-    private String externalToken_cardBrandName;
-    private String externalToken_cardAcquirerId;
-    private String externalToken_cardAcquirerName;
-    private String externalToken_blockedCard;
-    private String externalToken_extendedCardType;
-    private String externalToken_clubName;
-    private String externalToken_Indication;
-    private String externalToken_tokenProvider;
+    private String blockedCard;
+    private String cardAcquirerId;
+    private String cardAcquirerName;
+    private String cardBin;
+    private String cardBrandId;
+    private String cardBrandName;
+    private String cardExpiration;
+    private String cardLength;
+    private String cardMask;
+    private String cardName;
+    private String cardTypeId;
+    private String cardTypeName;
+    private String clubName;
+    private String creditCompanyId;
+    private String creditCompanyName;
+    private String extendedCardType;
+    private String Indication;
+    private String tokenValue;
+    private String tokenProvider;
 
-    public String getExternalToken_tokenValue() {
-        return externalToken_tokenValue;
+    public String getBlockedCard() {
+        return blockedCard;
     }
 
-    public void setExternalToken_tokenValue(String externalToken_tokenValue) {
-        this.externalToken_tokenValue = externalToken_tokenValue;
+    public void setBlockedCard(String blockedCard) {
+        this.blockedCard = blockedCard;
     }
 
-    public String getExternalToken_cardBin() {
-        return externalToken_cardBin;
+    public String getCardAcquirerId() {
+        return cardAcquirerId;
     }
 
-    public void setExternalToken_cardBin(String externalToken_cardBin) {
-        this.externalToken_cardBin = externalToken_cardBin;
+    public void setCardAcquirerId(String cardAcquirerId) {
+        this.cardAcquirerId = cardAcquirerId;
     }
 
-    public String getExternalToken_cardMask() {
-        return externalToken_cardMask;
+    public String getCardAcquirerName() {
+        return cardAcquirerName;
     }
 
-    public void setExternalToken_cardMask(String externalToken_cardMask) {
-        this.externalToken_cardMask = externalToken_cardMask;
+    public void setCardAcquirerName(String cardAcquirerName) {
+        this.cardAcquirerName = cardAcquirerName;
     }
 
-    public String getExternalToken_cardLength() {
-        return externalToken_cardLength;
+    public String getCardBin() {
+        return cardBin;
     }
 
-    public void setExternalToken_cardLength(String externalToken_cardLength) {
-        this.externalToken_cardLength = externalToken_cardLength;
+    public void setCardBin(String cardBin) {
+        this.cardBin = cardBin;
     }
 
-    public String getExternalToken_cardName() {
-        return externalToken_cardName;
+    public String getCardBrandId() {
+        return cardBrandId;
     }
 
-    public void setExternalToken_cardName(String externalToken_cardName) {
-        this.externalToken_cardName = externalToken_cardName;
+    public void setCardBrandId(String cardBrandId) {
+        this.cardBrandId = cardBrandId;
     }
 
-    public String getExternalToken_cardExpiration() {
-        return externalToken_cardExpiration;
+    public String getCardBrandName() {
+        return cardBrandName;
     }
 
-    public void setExternalToken_cardExpiration(String externalToken_cardExpiration) {
-        this.externalToken_cardExpiration = externalToken_cardExpiration;
+    public void setCardBrandName(String cardBrandName) {
+        this.cardBrandName = cardBrandName;
     }
 
-    public String getExternalToken_cardTypeId() {
-        return externalToken_cardTypeId;
+    public String getCardExpiration() {
+        return cardExpiration;
     }
 
-    public void setExternalToken_cardTypeId(String externalToken_cardTypeId) {
-        this.externalToken_cardTypeId = externalToken_cardTypeId;
+    public void setCardExpiration(String cardExpiration) {
+        this.cardExpiration = cardExpiration;
     }
 
-    public String getExternalToken_cardTypeName() {
-        return externalToken_cardTypeName;
+    public String getCardLength() {
+        return cardLength;
     }
 
-    public void setExternalToken_cardTypeName(String externalToken_cardTypeName) {
-        this.externalToken_cardTypeName = externalToken_cardTypeName;
+    public void setCardLength(String cardLength) {
+        this.cardLength = cardLength;
     }
 
-    public String getExternalToken_creditCompanyId() {
-        return externalToken_creditCompanyId;
+    public String getCardMask() {
+        return cardMask;
     }
 
-    public void setExternalToken_creditCompanyId(String externalToken_creditCompanyId) {
-        this.externalToken_creditCompanyId = externalToken_creditCompanyId;
+    public void setCardMask(String cardMask) {
+        this.cardMask = cardMask;
     }
 
-    public String getExternalToken_creditCompanyName() {
-        return externalToken_creditCompanyName;
+    public String getCardName() {
+        return cardName;
     }
 
-    public void setExternalToken_creditCompanyName(String externalToken_creditCompanyName) {
-        this.externalToken_creditCompanyName = externalToken_creditCompanyName;
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
     }
 
-    public String getExternalToken_cardBrandId() {
-        return externalToken_cardBrandId;
+    public String getCardTypeId() {
+        return cardTypeId;
     }
 
-    public void setExternalToken_cardBrandId(String externalToken_cardBrandId) {
-        this.externalToken_cardBrandId = externalToken_cardBrandId;
+    public void setCardTypeId(String cardTypeId) {
+        this.cardTypeId = cardTypeId;
     }
 
-    public String getExternalToken_cardBrandName() {
-        return externalToken_cardBrandName;
+    public String getCardTypeName() {
+        return cardTypeName;
     }
 
-    public void setExternalToken_cardBrandName(String externalToken_cardBrandName) {
-        this.externalToken_cardBrandName = externalToken_cardBrandName;
+    public void setCardTypeName(String cardTypeName) {
+        this.cardTypeName = cardTypeName;
     }
 
-    public String getExternalToken_cardAcquirerId() {
-        return externalToken_cardAcquirerId;
+    public String getClubName() {
+        return clubName;
     }
 
-    public void setExternalToken_cardAcquirerId(String externalToken_cardAcquirerId) {
-        this.externalToken_cardAcquirerId = externalToken_cardAcquirerId;
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
     }
 
-    public String getExternalToken_cardAcquirerName() {
-        return externalToken_cardAcquirerName;
+    public String getCreditCompanyId() {
+        return creditCompanyId;
     }
 
-    public void setExternalToken_cardAcquirerName(String externalToken_cardAcquirerName) {
-        this.externalToken_cardAcquirerName = externalToken_cardAcquirerName;
+    public void setCreditCompanyId(String creditCompanyId) {
+        this.creditCompanyId = creditCompanyId;
     }
 
-    public String getExternalToken_blockedCard() {
-        return externalToken_blockedCard;
+    public String getCreditCompanyName() {
+        return creditCompanyName;
     }
 
-    public void setExternalToken_blockedCard(String externalToken_blockedCard) {
-        this.externalToken_blockedCard = externalToken_blockedCard;
+    public void setCreditCompanyName(String creditCompanyName) {
+        this.creditCompanyName = creditCompanyName;
     }
 
-    public String getExternalToken_extendedCardType() {
-        return externalToken_extendedCardType;
+    public String getExtendedCardType() {
+        return extendedCardType;
     }
 
-    public void setExternalToken_extendedCardType(String externalToken_extendedCardType) {
-        this.externalToken_extendedCardType = externalToken_extendedCardType;
+    public void setExtendedCardType(String extendedCardType) {
+        this.extendedCardType = extendedCardType;
     }
 
-    public String getExternalToken_clubName() {
-        return externalToken_clubName;
+    public String getIndication() {
+        return Indication;
     }
 
-    public void setExternalToken_clubName(String externalToken_clubName) {
-        this.externalToken_clubName = externalToken_clubName;
+    public void setIndication(String indication) {
+        Indication = indication;
     }
 
-    public String getExternalToken_Indication() {
-        return externalToken_Indication;
+    public String getTokenValue() {
+        return tokenValue;
     }
 
-    public void setExternalToken_Indication(String externalToken_Indication) {
-        this.externalToken_Indication = externalToken_Indication;
+    public void setTokenValue(String tokenValue) {
+        this.tokenValue = tokenValue;
     }
 
-    public String getExternalToken_tokenProvider() {
-        return externalToken_tokenProvider;
+    public String getTokenProvider() {
+        return tokenProvider;
     }
 
-    public void setExternalToken_tokenProvider(String externalToken_tokenProvider) {
-        this.externalToken_tokenProvider = externalToken_tokenProvider;
+    public void setTokenProvider(String tokenProvider) {
+        this.tokenProvider = tokenProvider;
     }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("ExternalToken{");
-        sb.append("externalToken_tokenValue='")
-                .append(externalToken_tokenValue)
-                .append('\'');
-        sb.append(", externalToken_cardBin='")
-                .append(externalToken_cardBin)
-                .append('\'');
-        sb.append(", externalToken_cardMask='")
-                .append(externalToken_cardMask)
-                .append('\'');
-        sb.append(", externalToken_cardLength='")
-                .append(externalToken_cardLength)
-                .append('\'');
-        sb.append(", externalToken_cardName='")
-                .append(externalToken_cardName)
-                .append('\'');
-        sb.append(", externalToken_cardExpiration='")
-                .append(externalToken_cardExpiration)
-                .append('\'');
-        sb.append(", externalToken_cardTypeId='")
-                .append(externalToken_cardTypeId)
-                .append('\'');
-        sb.append(", externalToken_cardTypeName='")
-                .append(externalToken_cardTypeName)
-                .append('\'');
-        sb.append(", externalToken_creditCompanyId='")
-                .append(externalToken_creditCompanyId)
-                .append('\'');
-        sb.append(", externalToken_creditCompanyName='")
-                .append(externalToken_creditCompanyName)
-                .append('\'');
-        sb.append(", externalToken_cardBrandId='")
-                .append(externalToken_cardBrandId)
-                .append('\'');
-        sb.append(", externalToken_cardBrandName='")
-                .append(externalToken_cardBrandName)
-                .append('\'');
-        sb.append(", externalToken_cardAcquirerId='")
-                .append(externalToken_cardAcquirerId)
-                .append('\'');
-        sb.append(", externalToken_cardAcquirerName='")
-                .append(externalToken_cardAcquirerName)
-                .append('\'');
-        sb.append(", externalToken_blockedCard='")
-                .append(externalToken_blockedCard)
-                .append('\'');
-        sb.append(", externalToken_extendedCardType='")
-                .append(externalToken_extendedCardType)
-                .append('\'');
-        sb.append(", externalToken_clubName='")
-                .append(externalToken_clubName)
-                .append('\'');
-        sb.append(", externalToken_Indication='")
-                .append(externalToken_Indication)
-                .append('\'');
-        sb.append(", externalToken_tokenProvider='")
-                .append(externalToken_tokenProvider)
-                .append('\'');
+        sb.append("blockedCard='").append(blockedCard).append('\'');
+        sb.append(", cardAcquirerId='").append(cardAcquirerId).append('\'');
+        sb.append(", cardAcquirerName='").append(cardAcquirerName).append('\'');
+        sb.append(", cardBin='").append(cardBin).append('\'');
+        sb.append(", cardBrandId='").append(cardBrandId).append('\'');
+        sb.append(", cardBrandName='").append(cardBrandName).append('\'');
+        sb.append(", cardExpiration='").append(cardExpiration).append('\'');
+        sb.append(", cardLength='").append(cardLength).append('\'');
+        sb.append(", cardMask='").append(cardMask).append('\'');
+        sb.append(", cardName='").append(cardName).append('\'');
+        sb.append(", cardTypeId='").append(cardTypeId).append('\'');
+        sb.append(", cardTypeName='").append(cardTypeName).append('\'');
+        sb.append(", clubName='").append(clubName).append('\'');
+        sb.append(", creditCompanyId='").append(creditCompanyId).append('\'');
+        sb.append(", creditCompanyName='").append(creditCompanyName).append('\'');
+        sb.append(", extendedCardType='").append(extendedCardType).append('\'');
+        sb.append(", Indication='").append(Indication).append('\'');
+        sb.append(", tokenValue='").append(tokenValue).append('\'');
+        sb.append(", tokenProvider='").append(tokenProvider).append('\'');
         sb.append('}');
         return sb.toString();
     }
